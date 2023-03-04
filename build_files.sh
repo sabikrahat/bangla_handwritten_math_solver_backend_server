1c7824396924012b522b2264cf10829945c2a832
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2023-03-04 13:52:15 +0600

    build.sh python version edit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.8.5 -m pip install -r requirements.txt
-python3.8.5 manage.py collectstatic --noinput --clear
+python3.8 -m pip install -r requirements.txt
+python3.8 manage.py collectstatic --noinput --clear
 echo "BUILD END"
 
